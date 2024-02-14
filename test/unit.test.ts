8dbaa49c53d12f81e0a3b0d68a7a560203de44c6
Tom Kirkpatrick <tom@strike.me>
2024-02-14 21:40:05 +0000

    Add tests for filterEstimates

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { addFeeEstimates } from "../src/util";
+import { addFeeEstimates, filterEstimates } from "../src/util";
 
 test("addFeeEstimates", () => {
   const feeByBlockTarget: FeeByBlockTarget = { 1: 500, 2: 400, 3: 300 };
@@ -14,4 +14,16 @@ test("addFeeEstimates", () => {
   expect(feeByBlockTarget[5]).toBeUndefined();
   expect(feeByBlockTarget[6]).toEqual(250);
   expect(Object.keys(feeByBlockTarget).length).toEqual(4);
+});
+
+test("filterEstimates", () => {
+  const feeByBlockTarget: FeeByBlockTarget = { 1: 500, 2: 400, 3: 300 };
+  const minFee = 350;
+
+  const result = filterEstimates(feeByBlockTarget, minFee);
+
+  expect(result[1]).toEqual(500);
+  expect(result[2]).toEqual(400);
+  expect(result[3]).toBeUndefined();
+  expect(Object.keys(result).length).toEqual(2);
 });
