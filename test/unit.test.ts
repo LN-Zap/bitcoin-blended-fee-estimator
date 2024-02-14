9f0e0fddaf297912642f43874e74c268fbfbb33f
Tom Kirkpatrick <tom@strike.me>
2024-02-14 21:39:14 +0000

    Improve addFeeEstimates tests

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -2,13 +2,16 @@ import { expect, test } from "bun:test";
 import { addFeeEstimates } from "../src/util";
 
 test("addFeeEstimates", () => {
-  const feeByBlockTarget: FeeByBlockTarget = { 1: 100, 2: 200, 3: 300 };
-  const newEstimates: FeeByBlockTarget = { 4: 50, 5: 150, 6: 250 };
+  const feeByBlockTarget: FeeByBlockTarget = { 1: 500, 2: 400, 3: 300 };
+  const newEstimates: FeeByBlockTarget = { 4: 320, 5: 300, 6: 250 };
 
   addFeeEstimates(feeByBlockTarget, newEstimates);
 
-  expect(feeByBlockTarget[4]).toEqual(50);
+  expect(feeByBlockTarget[1]).toEqual(500);
+  expect(feeByBlockTarget[2]).toEqual(400);
+  expect(feeByBlockTarget[3]).toEqual(300);
+  expect(feeByBlockTarget[4]).toBeUndefined();
   expect(feeByBlockTarget[5]).toBeUndefined();
-  expect(feeByBlockTarget[6]).toBeUndefined();
+  expect(feeByBlockTarget[6]).toEqual(250);
   expect(Object.keys(feeByBlockTarget).length).toEqual(4);
 });
