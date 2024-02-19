417d7f611630bb439ded7b0baae0565001c7ae76
Tom Kirkpatrick <tom@strike.me>
2024-02-19 00:13:05 +0000

    Add comments to mempool provider

--- src/mempool.ts
+++ src/mempool.ts
@@ -3,116 +3,134 @@ import { logger } from "./logger";
 
 const log = logger(LOGLEVEL);
 
+/**
+ * MempoolProvider class implements the Provider interface.
+ * It provides methods to fetch data from a Mempool API.
+ */
 export class MempoolProvider implements Provider {
-  private url: string;
-  private depth: number;
-  private timeout: number;
+    private url: string;
+    private depth: number;
+    private timeout: number;
 
-  constructor(
-    url: string,
-    defaultDepth: number,
-    defaultTimeout: number = 5000,
-  ) {
-    this.url = url;
-    this.depth = defaultDepth;
-    this.timeout = defaultTimeout;
-  }
-
-  async getFeeEstimates(
-    maxDepth: number = this.depth,
-  ): Promise<FeeByBlockTarget> {
-    try {
-      const response = await fetchWithTimeout(
-        `${this.url}/api/v1/fees/recommended`,
-        this.timeout,
-      );
-      const data = (await response.json()) as MempoolFeeEstimates;
-
-      if (!data.fastestFee || !data.halfHourFee || !data.hourFee) {
-        throw new Error("Invalid fee data");
-      }
-
-      const feeEstimates: FeeByBlockTarget = this.transformFeeData(
-        data,
-        maxDepth,
-      );
-      return feeEstimates;
-    } catch (error) {
-      log.error({ msg: "Error getting fee estimates from Mempool:", error });
-      throw error;
+    /**
+     * Constructs a new MempoolProvider.
+     * @param url - The base URL of the Mempool API.
+     * @param defaultDepth - The default depth for fee estimates.
+     * @param defaultTimeout - The default timeout for fetch requests.
+     */
+    constructor(
+        url: string,
+        defaultDepth: number,
+        defaultTimeout: number = 5000,
+    ) {
+        this.url = url;
+        this.depth = defaultDepth;
+        this.timeout = defaultTimeout;
     }
-  }
 
-  async getBlockHeight(): Promise<number> {
-    try {
-      const response = await fetchWithTimeout(
-        `${this.url}/api/blocks/tip/height`,
-        this.timeout,
-      );
-      const height = await response.text();
+    /**
+     * Fetches fee estimates from the Mempool API.
+     * @param maxDepth - The maximum depth for fee estimates.
+     * @returns A promise that resolves to an object of fee estimates.
+     */
+    async getFeeEstimates(maxDepth: number = this.depth): Promise<FeeByBlockTarget> {
+        const data = await this.fetchData<MempoolFeeEstimates>('/api/v1/fees/recommended', 'json');
 
-      if (isNaN(Number(height))) {
-        throw new Error("Invalid block height");
-      }
+        if (!data.fastestFee || !data.halfHourFee || !data.hourFee) {
+            throw new Error("Invalid fee data");
+        }
 
-      return Number(height);
-    } catch (error) {
-      log.error({ msg: "Error getting block height from Mempool:", error });
-      throw error;
+        const feeEstimates: FeeByBlockTarget = this.transformFeeData(data, maxDepth);
+        return feeEstimates;
     }
-  }
 
-  async getBlockHash(): Promise<string> {
-    try {
-      const response = await fetchWithTimeout(
-        `${this.url}/api/blocks/tip/hash`,
-        this.timeout,
-      );
-      const hash = await response.text();
+    /**
+     * Fetches the current block height from the Mempool API.
+     * @returns A promise that resolves to the current block height.
+     */
+    async getBlockHeight(): Promise<number> {
+        const height = await this.fetchData<string>('/api/blocks/tip/height', 'text');
 
-      // Bitcoin block hash is a 64-character long hexadecimal string
-      const hashRegex = /^[a-fA-F0-9]{64}$/;
+        if (isNaN(Number(height))) {
+            throw new Error("Invalid block height");
+        }
 
-      if (!hashRegex.test(hash)) {
-        throw new Error("Invalid block hash");
-      }
-
-      return hash;
-    } catch (error) {
-      log.error({ msg: "Error getting block hash from Mempool:", error });
-      throw error;
+        return Number(height);
     }
-  }
 
-  public async getAllData(): Promise<ProviderData> {
-    try {
-      const [blockHeight, blockHash, feeEstimates] = await Promise.all([
-        this.getBlockHeight(),
-        this.getBlockHash(),
-        this.getFeeEstimates(),
-      ]);
+    /**
+     * Fetches the current block hash from the Mempool API.
+     * @returns A promise that resolves to the current block hash.
+     */
+    async getBlockHash(): Promise<string> {
+        const hash = await this.fetchData<string>('/api/blocks/tip/hash', 'text');
 
-      return {
-        blockHeight,
-        blockHash,
-        feeEstimates,
-      };
-    } catch (error) {
-      log.error({ msg: "Error fetching all data from Mempool:", error });
-      throw error;
+        // Bitcoin block hash is a 64-character long hexadecimal string
+        const hashRegex = /^[a-fA-F0-9]{64}$/;
+
+        if (!hashRegex.test(hash)) {
+            throw new Error("Invalid block hash");
+        }
+
+        return hash;
     }
-  }
 
-  private transformFeeData(
-    data: MempoolFeeEstimates,
-    maxDepth: number,
-  ): FeeByBlockTarget {
-    const feeEstimates: FeeByBlockTarget = {};
+    /**
+     * Fetches all data (block height, block hash, and fee estimates) from the Mempool API.
+     * @returns A promise that resolves to an object of all data.
+     */
+    public async getAllData(): Promise<ProviderData> {
+        try {
+            const [blockHeight, blockHash, feeEstimates] = await Promise.all([
+                this.getBlockHeight(),
+                this.getBlockHash(),
+                this.getFeeEstimates(),
+            ]);
 
-    if (maxDepth >= 1) feeEstimates[1] = data.fastestFee;
-    if (maxDepth >= 3) feeEstimates[3] = data.halfHourFee;
-    if (maxDepth >= 6) feeEstimates[6] = data.hourFee;
+            return {
+                blockHeight,
+                blockHash,
+                feeEstimates,
+            };
+        } catch (error) {
+            log.error({ msg: "Error fetching all data from Mempool:", error });
+            throw error;
+        }
+    }
 
-    return feeEstimates;
-  }
+    /**
+     * Fetches data from a specific endpoint of the Mempool API.
+     * @param endpoint - The endpoint to fetch data from.
+     * @param responseType - The type of the response ('json' or 'text').
+     * @returns A promise that resolves to the fetched data.
+     */
+    private async fetchData<T>(endpoint: string, responseType: 'json' | 'text'): Promise<T> {
+        try {
+            const response = await fetchWithTimeout(`${this.url}${endpoint}`, this.timeout);
+            const data = await (responseType === 'json' ? response.json() : response.text());
+            return data as T;
+        } catch (error) {
+            log.error({ msg: `Error fetching data from ${endpoint}:`, error });
+            throw error;
+        }
+    }
+
+    /**
+     * Transforms the fetched fee data into a FeeByBlockTarget object.
+     * @param data - The fetched fee data.
+     * @param maxDepth - The maximum depth for fee estimates.
+     * @returns A FeeByBlockTarget object.
+     */
+    private transformFeeData(
+        data: MempoolFeeEstimates,
+        maxDepth: number,
+    ): FeeByBlockTarget {
+        const feeEstimates: FeeByBlockTarget = {};
+
+        if (maxDepth >= 1) feeEstimates[1] = data.fastestFee;
+        if (maxDepth >= 3) feeEstimates[3] = data.halfHourFee;
+        if (maxDepth >= 6) feeEstimates[6] = data.hourFee;
+
+        return feeEstimates;
+    }
 }
